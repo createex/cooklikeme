3dcbfbe5bc093ae548fee4fbf2530243473814ad
Shafiq Ahmad <officialshafiqahmad@gmail.com>
2024-12-19 00:32:59 +0500

    Web Config Added

--- config/dotenv.js
+++ config/dotenv.js
@@ -13,15 +13,3 @@ requiredEnvVars.forEach((envVar) => {
 });
 
 console.log('Environment variables loaded successfully');
-const dotenv = require('dotenv');
-dotenv.config(); 
-
-console.log('Environment variables loaded:');
-console.log(process.env);
-
-module.exports = {
-  MONGO_URI: process.env.MONGO_URI,
-  PORT: process.env.PORT || 5000,
-  JWT_SECRET: process.env.JWT_SECRET,
-  JWT_EXPIRATION: process.env.JWT_EXPIRATION || '1h',
-};
