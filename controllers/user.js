3a78ebf4467e7cb9a11e5ebe0c9b39690939002f
Shafiq Ahmad <officialshafiqahmad@gmail.com>
2024-12-23 15:08:50 +0500

    Remove token expiry

--- controllers/user.js
+++ controllers/user.js
@@ -32,7 +32,7 @@ const login = async (req, res) => {
     }
 
     // Create a JWT token
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
 
     return res.status(200).json({ message: 'Login successful', isOtpVerified: user.isOtpVerified, token, _id: user._id });
 
