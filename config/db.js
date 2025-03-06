499c19a3edc83d576c9cce046284b3729d8823d8
Shafiq Ahmad <officialshafiqahmad@gmail.com>
2025-03-06 05:43:54 +0500

    Database URL Statically configured

--- config/db.js
+++ config/db.js
@@ -3,11 +3,11 @@ const config = require('./config');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(config.MONGO_URI, {
+    await mongoose.connect("mongodb+srv://cooklikemeuser:QRqbdTttOpXpbKd0@cooklikeme.dglut.mongodb.net/?retryWrites=true&w=majority&appName=CookLikeMe", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB Connected Successfully' + config.MONGO_URI);
+    console.log('MongoDB Connected Successfully' + "mongodb+srv://cooklikemeuser:QRqbdTttOpXpbKd0@cooklikeme.dglut.mongodb.net/?retryWrites=true&w=majority&appName=CookLikeMe");
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
     process.exit(1);
